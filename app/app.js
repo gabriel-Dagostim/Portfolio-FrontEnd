0275bd3de35d0c9e859d7e2736d322b8899e2f86
Gabriel Henrique Dagostim Simões <129344321+gabriel-Dagostim@users.noreply.github.com>
2023-08-24 21:34:48 -0300

    up js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,23 @@
 //0c0b19
-
 //25f700
+
+const titleP = document.querySelector('#title-port').innerHTML;
+const titleHi = document.querySelector('hi-p').innerHTML;
+const titleHiT = document.querySelector('hi-p-2').innerHTML;
+
+const aboutMeTitle = document.getElementById('about-me-title').innerHTML;
+const aboutMetext = document.getElementById('about-me-text').innerHTML;
+
+
+const MyHabi = document.getElementById('myhab').innerHTML;
+
+const MyProjects = document.getElementById('MyProjects').innerHTML;
+
+const contactMe = document.getElementById('contact-me').innerHTML;
+var lang = "ee";
+
+function changeLang(lang){
+    alert(lang);
+
+    }
+
