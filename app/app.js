6213d294c37e351c6fcf5c098ed6b28dbe26ac99
Gabriel Henrique Dagostim Simões <129344321+gabriel-Dagostim@users.noreply.github.com>
2023-08-28 20:19:38 -0300

    Change lang

--- app/app.js
+++ app/app.js
@@ -1,23 +1,30 @@
 //0c0b19
 //25f700
 
-const titleP = document.querySelector('#title-port').innerHTML;
-const titleHi = document.querySelector('hi-p').innerHTML;
-const titleHiT = document.querySelector('hi-p-2').innerHTML;
 
-const aboutMeTitle = document.getElementById('about-me-title').innerHTML;
-const aboutMetext = document.getElementById('about-me-text').innerHTML;
+const checkbox = document.getElementById("button33");
+function clickAct(){
+    var isChecked = checkbox.checked;
+
+    if (isChecked) {       
+document.getElementById('title-port').innerHTML = "Portfólio";
+document.getElementById('hi-p').innerHTML = "Olá, me chamo Gabriel dagostim";
+document.getElementById('hi-p-2').innerHTML = "Programador front-end";
+document.getElementById('about-me-title').innerHTML = "Sobre mim";
+document.getElementById('about-me-text').innerHTML = "Desenvolvedor Front-end com experiência em JavaScript, Material UI e consumo de APIs. Apaixonado pelo desenvolvimento web. Também possuo experiência em projetos gerenciados por Metodologias Ágeis. Cursando engenharia de software com foco em especialização em Front-End na Fundação Assis Gurgacz";
+document.getElementById('myhab').innerHTML = "Minhas Habilidades";
+document.getElementById('MyProjects').innerHTML = "Meus projetos";
+document.getElementById('contact-me').innerHTML = "Contato";
+}else {       
+document.getElementById('title-port').innerHTML = "Portfolio";
+document.getElementById('hi-p').innerHTML = "Hello, my name is Gabriel dagostim";
+document.getElementById('hi-p-2').innerHTML = "Front-end programmer";
+document.getElementById('about-me-title').innerHTML = "About me";
+document.getElementById('about-me-text').innerHTML = "Front-end developer with experience in JavaScript, Material UI and API consumption. Passionate about web development. I also have experience in projects managed using Agile Methodologies. Studying software engineering with a specialization in Front-End at the Assis Gurgacz Foundation.";
+document.getElementById('myhab').innerHTML = "My Skills";
+document.getElementById('MyProjects').innerHTML = "My projects";
+document.getElementById('contact-me').innerHTML = "Contact";
+}}
 
 
-const MyHabi = document.getElementById('myhab').innerHTML;
-
-const MyProjects = document.getElementById('MyProjects').innerHTML;
-
-const contactMe = document.getElementById('contact-me').innerHTML;
-var lang = "ee";
-
-function changeLang(lang){
-    alert(lang);
-
-    }
 
